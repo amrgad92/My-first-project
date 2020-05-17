b03e660bbc4dea28d697d7e7bcb6d27516bb02b9
amrgad92 <amrgad608@gmail.com>
2020-05-17 16:39:08 +0200

    modifing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
-var x = myFunction(4, 3);   // Function is called, return value will end up in x
+let x = $("#scroll").offset().top;
+$(window).scroll(function(){
+  
+  let y = $(window).scrollTop();
 
-function myFunction(a, b) {
-  return a * b;             // Function returns the product of a and b
-}
+  if(y > x)
+  {
+    $(".arrow").fadeIn(500);
+
+    $("#nav").css( {"backgroundColor" : "#8e7754"} )
+  }
+  else
+  {
+    $(".arrow").fadeOut(500);
+    $("#nav").css( {"backgroundColor" : "transparent"} )
+  }
+})
+
+
+$(".arrow").click(function()
+{
+  $("html , body").animate({scrollTop:0}, 1500)
+})
+
+$("#home").click(function()
+{
+  $("body , html").animate({scrollTop:0} , 1500)
+})
+
+let scroll = $("#scroll").offset().top
+
+$("#about").click(function()
+{
+  $("body , html").animate({scrollTop: scroll} , 1500)
+})
+
+let scroll3 = $("#scroll3").offset().top
+
+$("#product").click(function()
+{
+  $("html , body").animate({scrollTop: scroll3} , 1500)
+})
+
+let scroll4 = $("#scroll4").offset().top
+
+$("#contact").click(function()
+{
+  $("html , body").animate({scrollTop: scroll4} , 1500)
+})
