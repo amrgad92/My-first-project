cf230c27d8a2bf76467bdd8fcc20847defba3cb7
Amr Gad <59254311+amrgad92@users.noreply.github.com>
2020-09-09 17:02:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,54 @@
-var	x=3,
-	y=5,
-	z=x+y;
-	console.log(z)
+let x = $("#scroll").offset().top;
+$(window).scroll(function(){
+  
+  let y = $(window).scrollTop();
+
+  if(y > x)
+  {
+    $(".arrow").fadeIn(500);
+
+    $("#nav").css( {"backgroundColor" : "#8e7754"} )
+  }
+  else
+  {
+    $(".arrow").fadeOut(500);
+    $("#nav").css( {"backgroundColor" : "transparent"} )
+  }
+})
+
+
+$(".arrow").click(function()
+{
+  $("html , body").animate({scrollTop:0}, 1500)
+})
+
+let scroll0 = $("#scroll0").offset().top
+
+$("#home").click(function()
+{
+  $("body , html").animate({scrollTop: scroll0} , 1500)
+})
+
+
+let scroll = $("#scroll").offset().top
+
+$("#about").click(function()
+{
+  $("body , html").animate({scrollTop: scroll} , 1500)
+})
+
+let scroll3 = $("#scroll3").offset().top
+
+$("#product").click(function()
+{
+  $("html , body").animate({scrollTop: scroll3} , 1500)
+})
+
+let scroll4 = $("#scroll4").offset().top
+
+$("#contact").click(function()
+{
+  $("html , body").animate({scrollTop: scroll4} , 1500)
+})
+
+
